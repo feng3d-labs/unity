fdb10c079c3ef2c9708a89621aad82f42bf593ea
feng <908087098@qq.com>
2020-04-10 19:01:48 +0800

    StateMachineBehaviour

--- src/animation/StateMachineBehaviour.ts
+++ src/animation/StateMachineBehaviour.ts
@@ -7,38 +7,65 @@ namespace feng3d.unity
     {
         /**
          * Called on the first Update frame when making a transition to a state machine. This is not called when making a transition into a state machine sub-state.
+         * 
+         * @param animator The Animator playing this state machine.
+         * @param stateMachinePathHash The full path hash for this state machine.
          */
-        OnStateMachineEnter
+        OnStateMachineEnter(animator: Animator, stateMachinePathHash: number)
+        {
+
+        }
 
         /**
          * Called on the last Update frame when making a transition out of a StateMachine. This is not called when making a transition into a StateMachine sub-state.
+         * 
+         * @param animator The Animator playing this state machine.
+         * @param stateMachinePathHash The full path hash for this state machine.
          */
-        OnStateMachineExit
+        OnStateMachineExit(animator: Animator, stateMachinePathHash: number)
+        {
+
+        }
 
         /**
          * Called on the first Update frame when a state machine evaluate this state.
          */
-        OnStateEnter
+        OnStateEnter(animator: Animator, animatorStateInfo: AnimatorStateInfo, layerIndex: number)
+        {
+
+        }
 
         /**
          * Called on the last update frame when a state machine evaluate this state.
          */
-        OnStateExit
+        OnStateExit(animator: Animator, animatorStateInfo: AnimatorStateInfo, layerIndex: number)
+        {
+
+        }
 
         /**
          * Called right after MonoBehaviour.OnAnimatorIK.
          */
-        OnStateIK
+        OnStateIK(animator: Animator, animatorStateInfo: AnimatorStateInfo, layerIndex: number)
+        {
+
+        }
 
         /**
          * Called right after MonoBehaviour.OnAnimatorMove.
          */
-        OnStateMove
+        OnStateMove(animator: Animator, animatorStateInfo: AnimatorStateInfo, layerIndex: number)
+        {
+
+        }
 
         /**
          * Called at each Update frame except for the first and last frame.
          */
-        OnStateUpdate
+        OnStateUpdate(animator: Animator, animatorStateInfo: AnimatorStateInfo, layerIndex: number)
+        {
+
+        }
 
     }
 }
