f688178ae28e7f121924e0bb13ad11b581e51c51
feng <908087098@qq.com>
2022-07-24 10:10:47 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+export * from './math/Rect.ts';
+        //
+export * from './enums/AvatarTarget.ts';
+export * from './enums/AvatarIKHint.ts';
+export * from './enums/AvatarIKGoal.ts';
+export * from './enums/HumanBodyBones.ts';
+export * from './enums/SendMessageOptions.ts';
+        //
+export * from './base/Bounds.ts';
+        //
+export * from './core/RectTransform.ts';
+export * from './core/BoneWeight.ts';
+export * from './core/Mesh.ts';
+        //
+export * from './core/Avatar.ts';
+export * from './core/Animator.ts';
+        //
+export * from './animation/enums/AnimatorRecorderMode.ts';
+export * from './animation/enums/AnimatorUpdateMode.ts';
+export * from './animation/enums/AnimatorCullingMode.ts';
+export * from './animation/enums/AnimatorControllerParameterType.ts';
+export * from './animation/enums/AnimationBlendMode.ts';
+export * from './animation/enums/AnimatorLayerBlendingMode.ts';
+export * from './animation/enums/AnimatorConditionMode.ts';
+export * from './animation/enums/TransitionInterruptionSource.ts';
+        //
+export * from './animation/MatchTargetWeightMask.ts';
+export * from './animation/AnimationEvent.ts';
+export * from './animation/StateMachineBehaviour.ts';
+export * from './animation/AnimatorStateInfo.ts';
+export * from './animation/AnimatorClipInfo.ts';
+export * from './animation/AnimationState.ts';
+export * from './animation/AnimationClip.ts';
+export * from './animation/RuntimeAnimatorController.ts';
+export * from './animation/AnimatorControllerParameter.ts';
+export * from './animation/EditorCurveBinding.ts';
+export * from './animation/AnimationClipCurveData.ts';
+export * from './animation/AnimatorController.ts';
+export * from './animation/AnimatorControllerLayer.ts';
+export * from './animation/AvatarMask.ts';
+export * from './animation/AvatarMaskBodyPart.ts';
+export * from './animation/AnimatorStateMachine.ts';
+export * from './animation/AnimatorTransitionBase.ts';
+export * from './animation/AnimatorStateTransition.ts';
+export * from './animation/AnimatorCondition.ts';
+export * from './animation/AnimatorState.ts';
+export * from './animation/Motion.ts';
+export * from './animation/AnimatorTransition.ts';
+export * from './animation/ChildAnimatorStateMachine.ts';
+export * from './animation/ChildAnimatorState.ts';
+        //
+export * from './playables/PlayableGraph.ts';
+        //
+export * from './shaders/Line_Trail.vertex.glsl.ts';
+export * from './shaders/Line_Trail.fragment.glsl.ts';
+export * from './shaders/Line_Trail.shader.ts';
+        //
+export * from './effects/enums/LineAlignment.ts';
+export * from './effects/enums/LineTextureMode.ts';
+export * from './effects/LineRenderer.ts';
+export * from './effects/TrailRenderer.ts';
+        //
+export * from './shaders/TransparentParticlesStandard.vertex.glsl.ts';
+export * from './shaders/TransparentParticlesStandard.fragment.glsl.ts';
+export * from './shaders/TransparentParticlesStandard.shader.ts';
